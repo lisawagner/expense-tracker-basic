626b70668d7a33ab2b7c5c04d13f654c0ac66e8e
Lisa Wagner <lisawagner.online@gmail.com>
2022-06-07 11:54:37 -0700

    Scaffolding for edit/update logic

--- src/components/BudgetList/BudgetList.js
+++ src/components/BudgetList/BudgetList.js
@@ -1,17 +1,25 @@
 import { useState } from 'react';
 import { useFirestore } from '../../hooks/useFirestore'
 import { MdDeleteForever, MdModeEdit } from 'react-icons/md';
+// components
+import EditForm from '../EditForm/EditForm'
 
 // styles
 import styles from './BudgetList.module.css'
 
 export default function BudgetList({ transactions }) {
+  const [open, setOpen] = useState({ edit:false, view:false })
   
   const { deleteDocument } = useFirestore('transactions')
   console.log('TRANSACTIONS: ', transactions);
 
+  const handleClose = () => {
+    setOpen({edit:false, view:false})
+  }
+
   const handleEdit = (docuData) => {
-    console.log("Handling edit for: ", docuData);
+    setOpen({...open, view: true})
+    console.log("Handling edit for: ", docuData, open);
   }
 
   return (
@@ -23,10 +31,15 @@ export default function BudgetList({ transactions }) {
             <p className={styles.name}>{transaction.name}</p>
             <p className={styles.amount}>${transaction.amount}</p>
             <button className={styles.deleteBtn} onClick={() => deleteDocument(transaction.id)}><MdDeleteForever/></button>
-            <button className={styles.updateBtn} onClick={() => handleEdit(transaction)}><MdModeEdit/></button>
+            <button className={styles.updateBtn} onClick={() => handleEdit(transaction)}>
+                <MdModeEdit/>
+            </button>
           </li>
         ))}
       </ul>
+      {open.edit &&
+        <EditForm />
+      }
     </div>
   )
 }
---
+++ src/components/EditForm/EditForm.js
@@ -0,0 +1,8 @@
+// styles
+import styles from './EditForm.module.css'
+
+export default function EditForm() {
+  return (
+    <div>EditForm</div>
+  )
+}
