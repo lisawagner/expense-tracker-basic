ddfa5dae3442bbc3a12509f6218c1c5d3b123d14
Lisa Wagner <lisawagner.online@gmail.com>
2022-06-07 21:00:51 -0700

    Clean up

--- src/components/BudgetList/BudgetList.js
+++ src/components/BudgetList/BudgetList.js
@@ -34,7 +34,7 @@ export default function BudgetList({ transactions }) {
             <button className={styles.updateBtn} onClick={() => handleEdit(transaction.id)}>
                 <MdModeEdit/>
             </button>
-            {/* NEED TO MOVE MODAL INTO INDIVIDUAL_ITEM */}
+            {/* NEED TO MOVE MODAL/EDIT_FORM INTO INDIVIDUAL_ITEM */}
             {showModal && (
             <Modal>
               {/* <EditForm transaction={transaction} /> */}
